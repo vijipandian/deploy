843ee00c198f837bfe603de254d4d01ea173619a
vijipandian <abharnapandian23@gmail.com>
2026-04-02 13:28:03 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import plotly.express as px
 
 
 users=pd.read_csv("/content/EduPro Online Platform.xlsx - Users.csv")
